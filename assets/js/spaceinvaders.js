7e401f9cfd54338fd18e267108571730b9492d13
RhysLewsley <rhysl@example.com>
2025-10-22 21:39:33 +0100

    add: player as spaceship image

--- assets/js/spaceinvaders.js
+++ assets/js/spaceinvaders.js
@@ -6,8 +6,11 @@ const CANVAS_HEIGHT = gameCanvas.height = 400;
 // // Load images
 const background = new Image();
 background.src = "assets/images/rhys/background.png";
+const playerImage = new Image();
+playerImage.src = "assets/images/rhys/ship/frame0.png";
 
 window.onload = () => {
     ctx.drawImage(background, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(playerImage, 275, 320, 50, 50);
 };
 
